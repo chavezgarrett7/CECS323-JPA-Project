2178ac8441e97fff4ed7caf40db010a790ac0fbb
chavezgarrett7 <chavezgarrett7@gmail.com>
2021-10-27 20:19:35 -0700

    Did the bulk of Authoring_entities

--- cecs323-jpa-car-club-main/src/main/java/csulb/cecs323/model/Authoring_entities.java
+++ cecs323-jpa-car-club-main/src/main/java/csulb/cecs323/model/Authoring_entities.java
@@ -23,4 +23,43 @@ public class Authoring_entities {
     @Column()
     private int year_formed;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAuthoring_entity_type() {
+        return authoring_entity_type;
+    }
+
+    public void setAuthoring_entity_type(String authoring_entity_type) {
+        this.authoring_entity_type = authoring_entity_type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHead_writer() {
+        return head_writer;
+    }
+
+    public void setHead_writer(String head_writer) {
+        this.head_writer = head_writer;
+    }
+
+    public int getYear_formed() {
+        return year_formed;
+    }
+
+    public void setYear_formed(int year_formed) {
+        this.year_formed = year_formed;
+    }
 }
